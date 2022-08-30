f367b6771c44bf914e9f6bc06cf021e49417fb78
matteo <matteo.grotto@athonet.com>
2022-08-30 18:05:41 +0200

    first commit

---
+++ lib/shop_checkout.ex
@@ -0,0 +1,18 @@
+defmodule ShopCheckout do
+  @moduledoc """
+  Documentation for `ShopCheckout`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ShopCheckout.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/shop_checkout_test.exs
@@ -0,0 +1,8 @@
+defmodule ShopCheckoutTest do
+  use ExUnit.Case
+  doctest ShopCheckout
+
+  test "greets the world" do
+    assert ShopCheckout.hello() == :world
+  end
+end
