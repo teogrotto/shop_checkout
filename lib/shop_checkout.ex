321d4ca7d29d19d91a0637af9d59601a599a47e1
matteo <matteo.grotto@athonet.com>
2022-08-30 23:11:05 +0200

    add checkout/1 function that accept a list of items

--- lib/shop_checkout.ex
+++ lib/shop_checkout.ex
@@ -4,15 +4,53 @@ defmodule ShopCheckout do
   """
 
   @doc """
-  Hello world.
+  Empty Cart
 
   ## Examples
 
-      iex> ShopCheckout.hello()
-      :world
+      iex> ShopCheckout.checkout([])
+      0.0
 
   """
-  def checkout(a) do
-    :world
+  def checkout(cart_list) do
+    Enum.reduce(cart_list, %{gr1: 0, sr1: 0, cf1: 0}, &beep/2)
+    |> count_total()
   end
+
+  # -----------------------------------------------
+
+  def count_total(%{gr1: gr1, sr1: sr1, cf1: cf1}) do
+    total_gr(gr1) + total_sr(sr1) + total_cf(cf1)
+  end
+
+  def beep(itemId, acc) do
+      Map.update!(acc, itemId, &(&1 + 1))
+  end
+
+  # -----------------------------------------------
+
+  def total_gr(gr1) do
+    (rem(gr1, 2)  + div(gr1, 2)) * 3.11
+    |> Float.round(2)
+  end
+
+  def total_sr(sr1) when sr1 >= 3 do
+    sr1 * 4.5
+    |> Float.round(2)
+  end
+  def total_sr(sr1) do
+    sr1 * 5.0
+    |> Float.round(2)
+  end
+
+  def total_cf(cf1) when cf1 >= 3 do
+    cf1 * (11.23 * 2 / 3 )
+    |> Float.round(2)
+  end;
+  def total_cf(cf1) do
+    cf1 * 11.23
+    |> Float.round(2)
+  end;
+
+
 end
