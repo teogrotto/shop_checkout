c0cd4d4603157da53a0b725ce99e1253ce072e83
matteo <matteo.grotto@athonet.com>
2022-08-30 23:09:52 +0200

    fix testcases

--- test/shop_checkout_test.exs
+++ test/shop_checkout_test.exs
@@ -2,19 +2,47 @@ defmodule ShopCheckoutTest do
   use ExUnit.Case
   doctest ShopCheckout
 
-  test "test1" do
+  test "basket1" do
     assert ShopCheckout.checkout([:gr1, :sr1, :gr1, :gr1, :cf1]) == 22.45
   end
 
-  test "test2" do
+  test "basket2" do
     assert ShopCheckout.checkout([:gr1, :gr1]) == 3.11
   end
 
-  test "test3" do
-    assert ShopCheckout.checkout([:sr1,:sr1,:sr1,:gr1]) == 16.61
+  test "basket3" do
+    assert ShopCheckout.checkout([:sr1,:sr1,:gr1,:sr1]) == 16.61
   end
 
-  test "test4" do
-    assert ShopCheckout.checkout([:gr1,:cf1,:gr1,:cf1,:cf1]) == 30.57
+  test "basket4" do
+    assert ShopCheckout.checkout([:gr1,:cf1,:sr1,:cf1,:cf1]) == 30.57
   end
+
+  test "green_tea" do
+    assert ShopCheckout.total_gr(0) == 0
+    assert ShopCheckout.total_gr(1) == 3.11
+    assert ShopCheckout.total_gr(2) == 3.11
+    assert ShopCheckout.total_gr(3) == 6.22
+    assert ShopCheckout.total_gr(4) == 6.22
+    assert ShopCheckout.total_gr(5) == 9.33
+  end
+
+  test "strawberries" do
+    assert ShopCheckout.total_sr(0) == 0
+    assert ShopCheckout.total_sr(1) == 5
+    assert ShopCheckout.total_sr(2) == 10
+    assert ShopCheckout.total_sr(3) == 13.50
+    assert ShopCheckout.total_sr(4) == 18
+    assert ShopCheckout.total_sr(5) == 22.50
+  end
+
+  test "coffe" do
+    assert ShopCheckout.total_cf(0) == 0
+    assert ShopCheckout.total_cf(1) == 11.23
+    assert ShopCheckout.total_cf(2) == 22.46
+    assert ShopCheckout.total_cf(3) == 22.46
+    assert ShopCheckout.total_cf(4) == 29.95
+    assert ShopCheckout.total_cf(5) == 37.43
+  end
+
 end
